a39254e4681995ef29f2d3ed0cda93e17f314dc1
WesleyM2510 <wesleymaciel1@live.com>
2021-11-19 09:34:32 -0300

    [frontend] adicionando funcionalidade para excluir comentarios

--- frontend/src/components/CommentBox.vue
+++ frontend/src/components/CommentBox.vue
@@ -12,6 +12,9 @@
             <b-button class="rating-button" variant="outline-dark" @click="countDislike">
               <p>&#128169;  {{ dislikesValue }}</p>
             </b-button>
+            <b-button class="rating-button" variant="outline-dark" @click="deleteComment">
+              <p>&#128465; </p>
+            </b-button>
         </div>
     </div>
     <div class="comment-container">
@@ -59,8 +62,31 @@ export default {
             this.dislikesValue += 1
           }
         })
-      }
+      },
+      deleteComment: function() {
+        
+      this.$swal({title:'Tem certeza de deseja excluir este  comentário',showCancelButton: true,
+        icon: 'question',  confirmButtonText: 'Quero prosseguir',
+        confirmButtonColor: '#3085d6',cancelButtonColor: '#d33',
+        cancelButtonText:"Deixa pra lá"}).then(response =>{
+          if(response.isConfirmed){
+            this.$http.post(this.$api_url + '/api/cadastro/deletar_comentario', {
+            id_comentario: this.id
+            }).then(response => {
+              if(response.data.status == 'success'){
+                location.reload();
+              }else if (response.data.message == 'Voce nao escreveu este comentario'){
+                                  
+                this.$swal({title:'Você não é o autor(a) deste comentário',
+                  icon: 'error',  confirmButtonText: 'Ok',
+                  confirmButtonColor: '#3085d6'
+                }) 
+              }
+            })
+         }
+      })
     }
+  }
 }
 </script>
 
--- frontend/src/views/disciplina/disciplina.js
+++ frontend/src/views/disciplina/disciplina.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import DisciplinaPagina from './DisciplinaPagina.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
 
 import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
